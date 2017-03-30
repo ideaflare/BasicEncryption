c9b6839dedbb282c7771ed4844ee697918c8a9c6
Christoph <ideaflare@gmail.com>
2017-03-30 10:39:01 +0200

    Demo encryption in console application

--- BasicEncryption/Program.fs
+++ BasicEncryption/Program.fs
@@ -1,8 +1,33 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+let usage () =
+    printfn "No file specified."
+    printfn "Usage: BasicEncryption.exe fileName"
+
+let encrypt text =
+    let rec jumble n text =
+        let transposed = TextTransposition.transpose 3 text
+        match n with
+        | 0 -> transposed
+        | n -> jumble (n - 1) transposed
+    let cypher = jumble 7 text
+    printfn "\nencrypted text:\n\n%s" cypher
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+
+    let maybeFilePath = argv |> List.ofArray |> List.tryHead
+
+    match maybeFilePath with
+    | None -> usage ()
+    | Some(filePath) ->
+        try
+            let stringText = System.IO.File.ReadAllText(filePath)
+            encrypt stringText
+        with
+            | :? System.IO.FileNotFoundException -> printfn "Couldn't find file %s" filePath
+            | e -> printfn "Error reading text: %A" e
+
     0 // return an integer exit code
           
--- BasicEncryption/Transposition.fs
+++ BasicEncryption/Transposition.fs
@@ -3,5 +3,5 @@
 let transpose columns items =    
     if columns < 2 then items else
     [0 .. columns - 1]
-    |> List.collect (fun colStart   -> [colStart .. columns .. List.length items  - 1])
+    |> List.collect (fun colStart -> [colStart .. columns .. List.length items - 1])
     |> List.map (fun i -> items.[i])
