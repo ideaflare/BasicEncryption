bd1a6e478f1574f5a12e2c731b15e70667512ed2
Christoph <ideaflare@gmail.com>
2017-03-30 14:27:05 +0200

    Add transpose reverse operation

--- BasicEncryption/Transposition.fs
+++ BasicEncryption/Transposition.fs
@@ -5,3 +5,11 @@ let transpose columns items =
     [0 .. columns - 1]
     |> List.collect (fun colStart -> [colStart .. columns .. List.length items - 1])
     |> List.map (fun i -> items.[i])
+    
+let undo columns (items : 'a list) =    
+    if columns < 2 then items else
+    [0 .. columns - 1]
+    |> List.collect (fun colStart -> [colStart .. columns .. List.length items - 1])
+    |> List.mapi (fun i x -> (i,x))
+    |> List.sortBy (fun (position, order) -> order)
+    |> List.map (fun (i, _) -> items.[i])
