18b4ca8dea7b96e8afa4703df1808a38fa2f6d42
Christoph <ideaflare@gmail.com>
2017-04-07 22:10:18 +0200

    Remove fluff in fsx test

--- BasicEncryption/Dev.fsx
+++ BasicEncryption/Dev.fsx
@@ -17,7 +17,7 @@ let decrypted = fix (charList "sc\{)xcl)c)JA&&Jo)JcMf*")
 
 decrypted |> stringFromCharList = testText
 
-// --------------------------------------------
+// transposition --------------------------------
 
 let filePath = """C:\MyTemp\findme.txt"""
 let stringText = System.IO.File.ReadAllText(filePath)
@@ -25,56 +25,6 @@ let stringText = System.IO.File.ReadAllText(filePath)
 printfn "size: %i content: %A" stringText.Length stringText
 
 "Myaaillba d tearh lt m" = transpose 3 stringText
-
-
-(* Now to reverse the transposition without the padding, first stab: *)
-
-
-//let todo2 = (listText text) |> List.groupBy
-
-let cypher = charList "Myaaillba d tea?rh lt m"
-
-let rows = 3
-let spills = cypher.Length % rows
-let rowLength = cypher.Length / rows
-
-let rec getSpills i acc text spillLength =
-    let rec addSpills i acc text spillLength =
-        match i with
-        | 0 -> acc
-        | n ->
-            let nextSpill = text |> List.take spillLength
-            let nextText = text |> List.skip spillLength
-            addSpills (n - 1) (nextSpill :: acc) nextText spillLength
-    addSpills i acc text spillLength
-    |> List.rev
-
-let spillRows = getSpills spills [] cypher (rowLength + 1)
-
-let rest = cypher |> List.skip (spills * (rowLength + 1))
-
-let normalRows = rest |> List.chunkBySize rowLength
-
-let allRows = spillRows @ normalRows
-
-[0 .. rowLength]
-|> List.collect (fun col ->     
-     [0 .. (rows - 1)]
-     |> List.map (fun row ->
-     if (col = rowLength && row >= spills) then
-        (-1,-1)
-     else (col,row)))
-     
-[0 .. rowLength]
-|> List.collect (fun col ->     
-     [0 .. (rows - 1)]
-     |> List.map (fun row ->
-     if (col = rowLength && row >= spills)
-     then None
-     else Some(allRows.[row].[col]) ))
-|> List.choose id
-|> List.map (fun c -> string c)
-|> List.reduce (+)
 
 
 
