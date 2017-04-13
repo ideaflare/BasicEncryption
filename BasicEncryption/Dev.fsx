a2d710dd11d99405dd111a01fe5bab2128414728
Christoph <ideaflare@gmail.com>
2017-04-13 07:37:28 +0200

    Add rotation fsx tests

--- BasicEncryption/Dev.fsx
+++ BasicEncryption/Dev.fsx
@@ -6,16 +6,24 @@
 open Domain
 open TextOperations
 open Substitution
+open Rotation
+
+// Todo
+// 1. Call tests with test framework / tasks
+// 2. Property based tests all the things \(",)/
+
+// general test setup
+let testPlainText = "Mary had a little lamb?"
+let testCharList = testPlainText |> charList
 
 // substitution -------------------------------
 
 let sub = substituteList keymap replaceMap;;
 let fix = substituteList replaceMap keymap;
-let testText = "Mary had a little lamb?"
-let encoded = sub (charList testText)
+let encoded = sub testCharList
 let decrypted = fix (charList "sc\{)xcl)c)JA&&Jo)JcMf*")
 
-decrypted |> stringFromCharList = testText
+decrypted |> stringFromCharList = testPlainText
 
 // transposition --------------------------------
 
@@ -26,7 +34,15 @@ printfn "size: %i content: %A" stringText.Length stringText
 
 "Myaaillba d tearh lt m" = transpose 3 stringText
 
+// rotation ----------------------------------
 
+let emptyInput : int list = []
+
+let wrapLeft = Rotation.apply
+let wrapRight = Rotation.undo //unapply ?
+wrapLeft 0 emptyInput = wrapLeft -3 emptyInput
+wrapLeft System.Int32.MinValue emptyInput = wrapLeft System.Int32.MaxValue emptyInput
+wrapRight 5 (wrapLeft 5 testCharList) = testCharList
 
 
 
