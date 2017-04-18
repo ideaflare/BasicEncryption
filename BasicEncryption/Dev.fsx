6c72d7fa768f4d250412ab225fcd6e9035686113
Christoph <ideaflare@gmail.com>
2017-04-18 21:18:07 +0200

    Build randomized keymap and test based on property instead of hard-coded value

--- BasicEncryption/Dev.fsx
+++ BasicEncryption/Dev.fsx
@@ -18,10 +18,17 @@ let testCharList = testPlainText |> charList
 
 // substitution -------------------------------
 
-let sub = substituteList keymap replaceMap;;
-let fix = substituteList replaceMap keymap;
-let encoded = sub testCharList
-let decrypted = fix (charList "sc\{)xcl)c)JA&&Jo)JcMf*")
+let testKeymap = buildKeyMap testCharList
+
+let testReplaceMap =
+    testKeymap
+    |> (Transposition.apply 3)
+    |> (Rotation.apply 2)
+
+let sub = substituteList testKeymap testReplaceMap;;
+let fix = substituteList testReplaceMap testKeymap;
+let encoded = sub testCharList 
+let decrypted = fix encoded
 
 decrypted |> stringFromCharList = testPlainText
 
@@ -42,8 +49,9 @@ let wrapLeft = Rotation.apply
 let wrapRight = Rotation.undo //unapply ?
 wrapLeft 0 emptyInput = wrapLeft -3 emptyInput
 wrapLeft System.Int32.MinValue emptyInput = wrapLeft System.Int32.MaxValue emptyInput
-wrapRight 5 (wrapLeft 5 testCharList) = testCharList
+wrapLeft 0 [1..5] = [1..5]
 
+wrapRight 5 (wrapLeft 5 testCharList) = testCharList
 
 
 
--- BasicEncryption/Substitution.fs
+++ BasicEncryption/Substitution.fs
@@ -5,18 +5,18 @@ let substituteList keyMap (replaceMap : char list) items =
         keyMap
         |> List.findIndex (fun k -> k = c)
         |> (fun i -> replaceMap.[i])
-    items
-    |> List.map substitute
+    List.map substitute items
 
-let keymap =
+let initialKeyMap =
     charList """\n\t `-=~!@#$%^&*()_+[]{}|\;':",./<>?"""
     @ ['0' .. '9']
     @ ['a' .. 'z']
     @ ['A' .. 'Z']
 
-let replaceMap =
-    keymap
-    |> (Transposition.apply 3)
-    |> (Rotation.apply 2)
+let buildKeyMap (input : char list) =
+    let rnd = System.Random()
+    initialKeyMap @ input
+    |> List.distinct
+    |> List.sortBy ( fun _ -> rnd.Next())
    
 
